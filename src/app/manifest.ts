20b2dfffd537d99b60f78721974ac455732a874d
Mihai Preda <m009mihai@gmail.com>
2026-04-03 10:20:53 +0300

    icon

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+short_name: "Brenado Construct",
+    description: "Aplicatie interna Brenado Construct pentru proiecte si administrare",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#0196ff",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  };
+}
